686d53695bdd1ce609ed3354d95787ff7c082b55
dendersen <dendersen@endersen.dk>
2023-10-11 10:58:01 +0200

    created general logic for the board class

--- src/app/Board.java
+++ src/app/Board.java
@@ -3,4 +3,12 @@ package app;
 public class Board {
   private Tile[] tiles;
   private RaffleCup cup;
+  public Board(int numberOfSides){
+    tiles = new Tile[numberOfSides*2-1];
+    cup = new RaffleCup(2, numberOfSides);
+  }
+  public Tile makeMove(){
+    cup.roll();
+    return tiles[cup.getSides()[0]+cup.getSides()[1]-1];
+  }
 }
