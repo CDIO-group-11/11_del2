40cb083544d6f23a01ff16661cd180b78d50db6a
dendersen <dendersen@endersen.dk>
2023-10-20 22:16:03 +0200

    starting work on new language reader

--- src/lang/Lang.java
+++ src/lang/Lang.java
@@ -8,7 +8,50 @@ import java.util.Scanner;
 import lang.Language.LanguageCode;
 
 public class Lang {
+  String[][] tiles;
+  public static void loadLang(LanguageCode lang){
+    ArrayList<String> tempText = new ArrayList<String>();
+    Scanner languageReader;
+    try {
+      languageReader = new Scanner(new File("./Lang/" + lang.name() + ".lang"));
+      String descriptorTile = "@";
+      int TileCount = 11;
+      String descriptorInterface = "&";
+      String descriptorErrorStart = "[";
+      String descriptorErrorEnd = "]";
+      {
+        String[] descriptor = languageReader.nextLine().split(" ");
+        if(descriptor.length == 3){
+          if(!descriptor[0].equals("")){
+            descriptorTile = descriptor[0];
+          }
+          try{
+            if(!descriptor[1].equals("")){
+              TileCount = Integer.parseInt(descriptor[1]);
+              }
+          }catch(NumberFormatException e){
+            System.out.println("tile count failed to read using default");
+          }
+          if(!descriptor[2].equals("")){
+            descriptorInterface = descriptor[2];
+          }
+        }
+      }
+        while(languageReader.hasNextLine()){
+        
+      }
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
   private static String text[][];
+  /**
+   * @deprecated
+   * @param lang
+   * @param textIndex
+   * @return
+   */
   public static String getText(LanguageCode lang, int textIndex) {
     if(text[lang.ordinal()] == null){
       try {
@@ -19,6 +62,11 @@ public class Lang {
     }
     return text[lang.ordinal()][textIndex];
   }
+  /**
+   * @deprecated
+   * @param lang
+   * @throws FileNotFoundException
+   */
   private static void generate(LanguageCode lang) throws FileNotFoundException {
     ArrayList<String> tempText = new ArrayList<String>();
     Scanner languageReader = new Scanner(new File("./Lang/" + lang.name() + ".lang"));
