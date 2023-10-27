1fdff7d4e26ba9634dbc9848452e38eebb65bf51
dendersen <dendersen@endersen.dk>
2023-10-27 12:45:45 +0200

    quick fix

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 javac -sourcepath ./src/ -d ./bin/ -nowarn ./src/app/Main.java
-echo if there are problems with wierd characters in the program use runClean.bat instead
+echo if there are problems with wierd characters/overlapping text, you shoud use runClean.bat instead
 pause
 java -cp ./bin/ app/Main
 Pause
