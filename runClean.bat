5a7e6fa9cca0cb07a5b144ca3390120583748076
dendersen <dendersen@endersen.dk>
2023-10-27 12:15:54 +0200

    added executables

--- runClean.bat
+++ runClean.bat
@@ -1,5 +1,6 @@
 @echo off
-javac -sourcepath ./src/ -d ./bin/ ./src/app/Main.java
-pause
+javac -sourcepath ./src/ -d ./bin/ -nowarn ./src/app/Main.java
+echo please try run.bat as it supports more features
+pause 
 java -cp ./bin/ app/Main clean
 Pause
